bad796baf1c06893ef89e67af1d784e41ebda3f8
Indrave <806344324@qq.com>
2018-03-14 21:06:08 +0800

    update:md5加密

--- src/main/java/com/ssmblog/core/util/MD5Util.java
+++ src/main/java/com/ssmblog/core/util/MD5Util.java
@@ -1,5 +1,7 @@
 package com.ssmblog.core.util;
 
+import java.security.MessageDigest;
+
 /**
  * @author wangyj
  * @date 2018/3/11
@@ -17,6 +19,31 @@ public class MD5Util {
         int d1 = n / 16;
         int d2 = n % 16;
         return hexDigits[d1] + hexDigits[d2];
+    }
+
+    //字节数组转为十六进制字符串
+    private static String byteArrayToHexString(byte b[]) {
+        StringBuffer resultSb = new StringBuffer();
+        for (int i = 0; i < b.length; i++)
+            resultSb.append(byteToHexString(b[i]));
+
+        return resultSb.toString();
+    }
+
+    public static String MD5Encode(String origin, String charsetname) {
+        String resultString = null;
+        try {
+            resultString = new String(origin);
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            if (charsetname == null || "".equals(charsetname))
+                resultString = byteArrayToHexString(md.digest(resultString
+                        .getBytes()));
+            else
+                resultString = byteArrayToHexString(md.digest(resultString
+                        .getBytes(charsetname)));
+        } catch (Exception exception) {
+        }
+        return resultString;
     }
 
 
