3934ce06307bfcf740c6b6ba1eb288c0633fb6e3
Indrave <806344324@qq.com>
2018-03-25 19:22:09 +0800

    add:加入添加图片功能

--- src/main/java/com/ssmblog/core/service/impl/PictureServiceImpl.java
+++ src/main/java/com/ssmblog/core/service/impl/PictureServiceImpl.java
@@ -1,8 +1,11 @@
 package com.ssmblog.core.service.impl;
 
+import com.ssmblog.core.dao.PictureDao;
 import com.ssmblog.core.entity.Picture;
 import com.ssmblog.core.service.PictureService;
+import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.List;
 import java.util.Map;
 
@@ -11,34 +14,45 @@ import java.util.Map;
  * @date 2018/3/24
  * @description
  */
+@Service("pictureService")
 public class PictureServiceImpl implements PictureService{
+
+    @Resource
+    private PictureDao pictureDao;
+
     @Override
     public List<Picture> findPicture(Map<String, Object> map) {
-        return null;
+        return pictureDao.findPictures(map);
     }
 
     @Override
     public Long getTotalPicture(Map<String, Object> map) {
-        return null;
+        return pictureDao.getTotalPictures(map);
     }
 
     @Override
     public int addPicture(Picture picture) {
-        return 0;
+        if (picture.getPath() == null || getTotalPicture(null) > 90 || picture.getPath().length() > 100 || picture.getUrl().length() > 100) {
+            return 0;
+        }
+        return pictureDao.addPicture(picture);
     }
 
     @Override
     public int updatePicture(Picture picture) {
-        return 0;
+        if (picture.getPath() == null || getTotalPicture(null) > 90 || picture.getPath().length() > 100 || picture.getUrl().length() > 100) {
+            return 0;
+        }
+        return pictureDao.updPicture(picture);
     }
 
     @Override
     public int deletePicture(String id) {
-        return 0;
+        return pictureDao.delPicture(id);
     }
 
     @Override
     public Picture findPictureById(String id) {
-        return null;
+        return pictureDao.findPictureById(id);
     }
 }
