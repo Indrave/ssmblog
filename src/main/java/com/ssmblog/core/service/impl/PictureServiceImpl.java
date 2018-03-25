87c8f64fb87ab6c9aef65a4bfb2db2db7e9f3e8a
Indrave <806344324@qq.com>
2018-03-25 19:12:08 +0800

    add:加入添加图片功能

--- src/main/java/com/ssmblog/core/service/impl/PictureServiceImpl.java
+++ src/main/java/com/ssmblog/core/service/impl/PictureServiceImpl.java
@@ -1,9 +1,44 @@
 package com.ssmblog.core.service.impl;
 
+import com.ssmblog.core.entity.Picture;
+import com.ssmblog.core.service.PictureService;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author wangyj
  * @date 2018/3/24
  * @description
  */
-public class PictureServiceImpl {
+public class PictureServiceImpl implements PictureService{
+    @Override
+    public List<Picture> findPicture(Map<String, Object> map) {
+        return null;
+    }
+
+    @Override
+    public Long getTotalPicture(Map<String, Object> map) {
+        return null;
+    }
+
+    @Override
+    public int addPicture(Picture picture) {
+        return 0;
+    }
+
+    @Override
+    public int updatePicture(Picture picture) {
+        return 0;
+    }
+
+    @Override
+    public int deletePicture(String id) {
+        return 0;
+    }
+
+    @Override
+    public Picture findPictureById(String id) {
+        return null;
+    }
 }
