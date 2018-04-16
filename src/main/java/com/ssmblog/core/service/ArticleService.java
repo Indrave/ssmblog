331d6b705e031fcf45af35bead968e812143ce11
Indrave <806344324@qq.com>
2018-04-16 22:26:28 +0800

    add:文章管理

--- src/main/java/com/ssmblog/core/service/ArticleService.java
+++ src/main/java/com/ssmblog/core/service/ArticleService.java
@@ -1,5 +1,10 @@
 package com.ssmblog.core.service;
 
+import com.ssmblog.core.entity.Article;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author wangyj
  * @date 2018/4/8
@@ -7,8 +12,51 @@ package com.ssmblog.core.service;
  */
 public interface ArticleService {
 
+    /**
+     * 查询文章
+     * @param map
+     * @return
+     */
+    public List<Article> findArticle(Map<String,Object> map);
 
+    /**
+     * 数据数目
+     *
+     * @param map
+     * @return
+     */
+    public Long getTotalArticle(Map<String, Object> map);
 
+    /**
+     * 添加文章
+     *
+     * @param article
+     * @return
+     */
+    public int addArticle(Article article);
 
+    /**
+     * 修改文章
+     *
+     * @param article
+     * @return
+     */
+    public int updateArticle(Article article);
+
+    /**
+     * 删除
+     *
+     * @param id
+     * @return
+     */
+    public int deleteArticle(String id);
+
+    /**
+     * 根据id查找
+     *
+     * @param id
+     * @return
+     */
+    public Article findById(String id);
 
 }
